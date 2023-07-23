5c5157362bcde862915071b696d6b99facd126cc
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-07-23 17:32:31 -0500

    Maven Publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     java
     `java-library`
     application
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "io.github.gaming32"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 application {
     mainClass.set("io.github.gaming32.prcraftinstaller.PrcraftInstaller")
@@ -20,13 +21,25 @@ repositories {
 }
 
 dependencies {
-    implementation("com.nitorcreations:javaxdelta:1.5")
-    implementation("net.fabricmc:tiny-remapper:0.8.7")
+    api("com.nitorcreations:javaxdelta:1.5")
+    api("net.fabricmc:tiny-remapper:0.8.7")
     api("org.apache.commons:commons-compress:1.23.0")
 }
 
 tasks.jar {
     manifest {
         attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "gaming32"
+            credentials(PasswordCredentials::class)
+
+            val baseUri = "https://maven.jemnetworks.com"
+            url = uri(baseUri + if (version.toString().endsWith("-SNAPSHOT")) "/snapshots" else "/releases")
+        }
     }
 }
